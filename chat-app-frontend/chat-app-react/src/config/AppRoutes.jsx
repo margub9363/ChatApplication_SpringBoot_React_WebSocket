fc42c48385f00d2fb51eabb376ca1643d3e90b7c
margub9363 <69838623+margub9363@users.noreply.github.com>
2026-01-17 19:32:26 +0530

    Configuring basic routes

---
+++ chat-app-frontend/chat-app-react/src/config/AppRoutes.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Route, Routes } from "react-router";
+import App from "../App";
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/chat" element={<h1>this is a chat pages</h1>} />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
