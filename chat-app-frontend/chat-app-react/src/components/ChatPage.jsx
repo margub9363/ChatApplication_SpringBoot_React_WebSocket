f8e5ebd5c09657134caaac8b883c8ae04ea7141b
margub9363 <69838623+margub9363@users.noreply.github.com>
2026-01-18 09:51:10 +0530

    till 50:50

--- chat-app-frontend/chat-app-react/src/components/ChatPage.jsx
+++ chat-app-frontend/chat-app-react/src/components/ChatPage.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const ChatPage = () => {
   return (
     <div className="">
-      <header className="dark:border-gray-700 shadow dark:bg-gray-900 py-5 flex justify-around item-center">
+      <header className="dark:border-gray-700 fixed h-20 w-full shadow dark:bg-gray-900 py-5 flex justify-around item-center">
         <div>
           <h1 className="text-xl font-semibold">
             Room : <span>Family Room</span>
@@ -20,6 +20,16 @@ const ChatPage = () => {
           </button>
         </div>
       </header>
+
+      <div className="fixed bottom-2 w-full h-16">
+        <div className="h-full border w-2/3 mx-auto dark:bg-gray-900 rounded">
+          <input
+            type="text"
+            placeholder="Type your message here..."
+            className="dark:border-gray-700 dark:bg-gray-900 px-3 py-2 rounded w-full h-full"
+          ></input>
+        </div>
+      </div>
     </div>
   );
 };
