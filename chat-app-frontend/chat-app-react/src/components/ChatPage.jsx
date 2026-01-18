669e4c40824bc8d626eb429edb82986e26bb8955
margub9363 <69838623+margub9363@users.noreply.github.com>
2026-01-18 11:39:45 +0530

    Till 1:20:23

--- chat-app-frontend/chat-app-react/src/components/ChatPage.jsx
+++ chat-app-frontend/chat-app-react/src/components/ChatPage.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { MdAttachFile, MdSend } from "react-icons/md";
+import Tannu from "../assets/Tannu.png";
 
 const ChatPage = () => {
+  const [messages, setMessages] = useState([
+    {
+      content: "hello",
+      sender: "Rahman",
+    },
+    {
+      content: "hii",
+      sender: "Tannu",
+    },
+    {
+      content: "hey",
+      sender: "Margub",
+    },
+    {
+      content: "yupp",
+      sender: "Rocky",
+    },
+  ]);
+  const [input, setInput] = useState("");
+  const inputRef = useRef(null);
+  const chatBoxRef = useRef(null);
+  const [stompClient, setStompClient] = useState(null);
+  const [roomId, setRoomId] = useState("");
+  const [currentUser] = useState("Rahman");
   return (
     <div className="">
       <header className="dark:border-gray-700 fixed h-20 w-full shadow dark:bg-gray-900 py-5 flex justify-around item-center">
@@ -22,40 +47,20 @@ const ChatPage = () => {
         </div>
       </header>
 
-      <main className="py-20 border w-2/3 dark:bg-slate-600 mx-auto h-screen overflow-auto">
-        <div className="message_container">
-          <h1>hii</h1>
-          <h1>hii</h1>
-          <h1>hii</h1>
-          <h1>hii</h1>
-          <h1>hii</h1>
-          <h1>hii</h1>
-          <h1>hii</h1>
-          <h1>hii</h1>
-          <h1>hii</h1>
-          <h1>hii</h1>
-          <h1>hii</h1>
-          <h1>hii</h1>
-          <h1>hii</h1>
-          <h1>hii</h1>
-          <h1>hii</h1>
-          <h1>hii</h1> <h1>hii</h1>
-          <h1>hii</h1>
-          <h1>hii</h1>
-          <h1>hii</h1>
-          <h1>hii</h1>
-          <h1>hii</h1>
-          <h1>hii</h1>
-          <h1>hii</h1>
-          <h1>hii</h1>
-          <h1>hii</h1>
-          <h1>hii</h1>
-          <h1>hii</h1>
-          <h1>hii</h1>
-          <h1>hii</h1>
-          <h1>hii</h1>
-          <h1>hii</h1>
-        </div>
+      <main className=" px-10 py-20 border w-2/3 dark:bg-slate-600 mx-auto h-screen overflow-auto">
+        {messages.map((message, index) => (
+          <div key={index} className="flex justify-start">
+            <div className="my-2 bg-blue-600 p-2 rounded max-w-xs">
+              <div className="flex flex-row">
+                <img className="h-10 w-10 gap-2" src={Tannu} alt="" />
+                <div className="flex flex-col gap-1">
+                  <p className="text-sm font-bold">{message.sender}</p>
+                  <p>{message.content}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
       </main>
       <div className="fixed bottom-4 w-full h-16">
         <div className="h-full pr-10 gap-4 flex items-center justify-between w-1/2 mx-auto dark:bg-gray-900 rounded-full">
